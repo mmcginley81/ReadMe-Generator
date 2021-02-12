b8da660296fcaa23360a44687cccd41eb2bff7b0
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-02-11 17:20:52 -0800

    establishing working files

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+Psuedo Code
+Assets needed - Description, Table of Contents, Instillation, Usage, License, Contribuiting, Test, Questions
+Need to ask for - Project title, description & above assets
+
+WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I choose a license for my application from a list of options
+THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+WHEN I enter my GitHub username
+THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+WHEN I enter my email address
+THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+WHEN I click on the links in the Table of Contents
+THEN I am taken to the corresponding section of the README
+*/
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+
+
+const questions = [
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
